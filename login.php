333b8157ecfd45aa04bf925ceac550b64429cf65
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-13 15:28:59 +0900

    エラー分岐　パスワードの暗号化解読　実装　11/13

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ try {
     $db = new PDO('mysql:dbname=conveni_db;host=127.0.0.1; charset=utf8', 'root', '');
 } catch (PDOException $e) {
     print('DB接続エラー：' . $e->getMessage());
+    exit();
 }
 
 if ($_COOKIE['email'] !== '') {
@@ -12,54 +13,64 @@ if ($_COOKIE['email'] !== '') {
 }
 
 if (!empty($_POST)) {
-    // パスワード入力あり
-    $email = $_POST['email'];
-    if ($_POST['email'] !== '' && $_POST['password'] !== '') {
-        $login = $db->prepare('SELECT * FROM m_users WHERE email=? AND password=?');
-        $login->execute(array($_POST['email'],$_POST['password']));
+    //ログイン失敗したら（一度チェックを通過するから）
+    $error['login'] = '* ログインに失敗しました';
 
+    $email = $_POST['email'];
+    // ＊メアドとパスワードが共に入力されているか
+    if ($_POST['email'] !== '' && $_POST['password'] !== '') {
+        $login = $db->prepare('SELECT * FROM m_users WHERE email=?');
+        $login->execute(array($_POST['email']));
         $member = $login->fetch();
 
-        if ($member['lock_flg'] === '0') {
-            if ($member) {
-                //ログイン成功
-                // ・ログイン失敗日時：null
-                // ・ログイン失敗回数：0
-                // ・ロック日時：null
-                // ・最終ログイン日時：現在日時
-                // ・ロックフラグ：0
-                // 上記５項目をupdateする
-                $login = $db->prepare('UPDATE m_users SET login_fail_date= "", login_fail_cut= 0, lock_date= "", last_login_date=?, lock_flg= 0 WHERE user_id=?');
-                $login->execute(array(date(Ymd),$member['user_id']));
+        // ＊保存パスワード　＝　入力パスワード かつ　ロック中ではない
+        if ($member['password'] === sha1($_POST['password']) && $member['lock_flg'] === '0') {
+            //ログイン成功：失敗項目の初期化
+            $login = $db->prepare('UPDATE m_users SET login_fail_date= null, login_fail_cut= 0, lock_date= null, last_login_date=?, lock_flg= 0 WHERE user_id=?');
+            $login->execute(array(time(),$member['user_id']));
 
-                // SESSIONにユーザーID保持
-                $_SESSION['user_id'] = $member['user_id'];
-                $_SESSION['time'] = time();
-                $_SESSION['user_name'] = $member['user_name'];
+            // SESSIONにユーザーID保持
+            $_SESSION['login']['user_id'] = $member['user_id'];
+            $_SESSION['login']['time'] = time();
+            $_SESSION['login']['user_name'] = $member['user_name'];
+            $_SESSION['login']['auth'] = $member['auth'];
+            // 初期化
+            $error['login'] = '';
+            header('Location: menu.php');
+            exit();
 
-                header('Location: menu.php');
-                exit();
-            } else {
-                //ログイン失敗
-                $error['login'] = 'failed';
-            }
-        } elseif ($member['lock_flg'] === '1' || $member['lock_flg'] === '9') {
-            //ロック中
-            $error['login'] = 'block';
+        } elseif($member['lock_flg'] === '1' || $member['lock_flg'] === '9') {
+            // ＊ロック中
+            $error['login'] = '* ロック中です。';
+
         } else {
-            //ログイン失敗
-            $error['login'] = 'failed';
+            // ＊パスワードが一致しない
+            $logmiss = $member['login_fail_cut'] + 1;
+            if($logmiss === 3){
+                // ３回ログインミスした
+                $penalty = $db->prepare('UPDATE m_users SET lock_flg=?, login_fail_cut=?, login_fail_date=? WHERE email=?');
+                $penalty->execute(array(1,$logmiss,time(),$_POST['email']));
+                $error['login'] = '* ログインに規定回数以上失敗したため、ロックしました。時間をおいて再ログインしてください。';
+            } else {
+                // ＊3回未満のログインミス
+                $penalty = $db->prepare('UPDATE m_users SET login_fail_cut=?, login_fail_date=? WHERE email=?');
+                $penalty->execute(array($logmiss,time(),$_POST['email']));
+            }
         }
+
     } else {
         // メールアドレスとパスワードの片方でも未入力
         if ($_POST['email'] === '') {
-            $message1['addr'] = 'noaddress';
+            // エラーメアド
+            $error['message'] .= '<br> メールアドレスが未入力です。';
         }
         if ($_POST['password'] === '') {
-            $message2['pass'] = 'nopass';
+            // エラーパスワード
+            $error['message'] .= '<br> パスワードが未入力です。';
         }
     }
 }
+unset($_SESSION['login']);
 ?>
 
 <!DOCTYPE html>
@@ -69,7 +80,7 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width,">
-    <title>タイトルを入力してください</title>
+    <title>商品管理システム【ログイン画面】</title>
     <!-- <link rel="stylesheet" href="style.css"> -->
     <style>
         html,
@@ -88,47 +99,29 @@ if (!empty($_POST)) {
 <body>
     <div>
         <a href="login.php">
-        <h1>画面タイトル</h1>
+        <h1>商品管理システム</h1>
         </a>
-        <p></p>
-        <hr>
-            <!-- メールアドレス、パスワード -->
-            <?php if ($message['addr'] !== 'noaddress' || $message['pass'] !== 'nopass') :?>
-                    <h4 style="color: red;">
-                <!-- ロック中　9：管理者側から　1：ログイン失敗 -->
-                <?php if ($error['login'] === 'block'): ?>
-                <p></p><p>* ロック中です。</p>
-                <?php endif; ?>
-                <!-- ログインに３回失敗 -->
-                <?php if ($error['login'] === 'lock'): ?>
-                <p>* ログインに規定回数以上失敗したため、ロックしました。時間をおいて再ログインしてください。</p>
-                <?php endif; ?>
-                <!-- メールアドレスが未入力 -->
-                <?php if ($message1['addr'] === 'noaddress'): ?>
-                <p>* メールアドレスが未入力です。</p>
-                <?php endif; ?>
-                <!-- パスワードが未入力です -->
-                <?php if ($message2['pass'] === 'nopass'): ?>
-                <p>* パスワードが未入力です。</p>
-                <?php endif; ?>
-                </h4>
-        <?php endif; ?>
 
+        <h3 style="color: red;">
         <!-- ログイン失敗 -->
-        <?php if ($error['login'] === 'failed'): ?>
-         <p></p><p>* ログインに失敗しました。</p>
+        <?php
+        if ($error['login'] !== ''):
+            print($error['login']);
+            if (!empty($error['message'])):
+                print($error['message']);
+            endif; ?>
         <?php endif; ?>
+        </h3>
 
-    <form method="post">
+    <form method="post" style="margin-top: 10%;">
         <div>
             <!-- メールアドレス入力 -->
-            <strong style="width: 200px;">メールアドレス　　　</strong>
+            <strong>メールアドレス　　　</strong>
             <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
             <br><br>
             <!-- パスワード入力 -->
-            <strong style="width: 200px;">パスワード　　　　　</strong>
+            <strong>パスワード　　　　　</strong>
             <input type="password" name="password" size="35" maxlength="16" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
-
         </div>
 
         <br><br>
@@ -139,6 +132,13 @@ if (!empty($_POST)) {
         </div>
     </form>
     </div>
+
+    <!-- 確認用（新規画面＆パスワードの暗号化コード完成まで付加） -->
+    <form action="menu.php">
+        <div>
+            <input type="submit" value="menu" style="margin-left: 70%; margin-top: 10%; float:left; width: 300px;">
+        </div>
+    </form>
 
 </body>
 </html>
