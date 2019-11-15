69961387d73645871d8bc4893de534a9f9805868
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-15 13:29:36 +0900

    11/15 更新 実装：DB接続ユーザー変更、時間差分条件

--- login.php
+++ login.php
@@ -2,14 +2,10 @@
 session_start();
 // DB接続
 try {
-    $db = new PDO('mysql:dbname=conveni_db;host=127.0.0.1; charset=utf8', 'root', '');
+    $db = new PDO('mysql:dbname=conveni_db;host=127.0.0.1; charset=utf8', 'daikichi', 'daiki');
 } catch (PDOException $e) {
     print('DB接続エラー：' . $e->getMessage());
     exit();
-}
-
-if ($_COOKIE['email'] !== '') {
-    $email = $_COOKIE['email'];
 }
 
 if (!empty($_POST)) {
@@ -23,41 +19,64 @@ if (!empty($_POST)) {
         $login->execute(array($_POST['email']));
         $member = $login->fetch();
 
-        // ＊保存パスワード　＝　入力パスワード かつ　ロック中ではない
-        if ($member['password'] === sha1($_POST['password']) && $member['lock_flg'] === '0') {
-            //ログイン成功：失敗項目の初期化
-            $login = $db->prepare('UPDATE m_users SET login_fail_date= null, login_fail_cut= 0, lock_date= null, last_login_date=?, lock_flg= 0 WHERE user_id=?');
-            $login->execute(array(time(),$member['user_id']));
+        // ログインしようとした時の日時
+        $today = new DateTime();
+        $misstime = $today->format('Y-m-d H:i:s');
+        if (!empty($member['login_fail_date'])) {
+            // 前回のログインミスの日時
+            $pastday = $member['login_fail_date'];
+            $pasttime = new DateTime($pastday);
+            // 差分計算
+            $interval['year'] = $pasttime->diff($today)->format("%y");    //年
+            $interval['month'] = $pasttime->diff($today)->format("%m");   //月
+            $interval['day'] = $pasttime->diff($today)->format("%d");     //日
+            $interval['hour'] = $pasttime->diff($today)->format("%h");    //時
+            $interval['minute'] = $pasttime->diff($today)->format("%i");  //分
+            // 「年,月,日,時」⇒「分」
+            $difference = ((($interval['year'] * 12 + $interval['month']) * 30 + $interval['day']) * 24 + $interval['hour']) * 60 + $interval['minute'];
 
-            // SESSIONにユーザーID保持
-            $_SESSION['login']['user_id'] = $member['user_id'];
-            $_SESSION['login']['time'] = time();
-            $_SESSION['login']['user_name'] = $member['user_name'];
-            $_SESSION['login']['auth'] = $member['auth'];
-            // 初期化
-            $error['login'] = '';
-            header('Location: menu.php');
-            exit();
-
-        } elseif($member['lock_flg'] === '1' || $member['lock_flg'] === '9') {
-            // ＊ロック中
-            $error['login'] = '* ロック中です。';
-
-        } else {
-            // ＊パスワードが一致しない
-            $logmiss = $member['login_fail_cut'] + 1;
-            if($logmiss === 3){
-                // ３回ログインミスした
-                $penalty = $db->prepare('UPDATE m_users SET lock_flg=?, login_fail_cut=?, login_fail_date=? WHERE email=?');
-                $penalty->execute(array(1,$logmiss,time(),$_POST['email']));
-                $error['login'] = '* ログインに規定回数以上失敗したため、ロックしました。時間をおいて再ログインしてください。';
-            } else {
-                // ＊3回未満のログインミス
-                $penalty = $db->prepare('UPDATE m_users SET login_fail_cut=?, login_fail_date=? WHERE email=?');
-                $penalty->execute(array($logmiss,time(),$_POST['email']));
+            // １０分以上経過ならばロックカウント、ロックフラグの初期化
+            if ($difference >= "10") {
+                $login = $db->prepare('UPDATE m_users SET login_fail_date= null, login_fail_cut= 0, lock_date= null, lock_flg= 0 WHERE user_id=?');
+                $login->execute(array($member['user_id']));
             }
         }
 
+
+        // ＊保存パスワード　＝　入力パスワード かつ　ロック中ではない時
+        if ($member['password'] === sha1($_POST['password']) && $member['lock_flg'] === '0') {
+            //ログイン成功：失敗項目の初期化
+            $login = $db->prepare('UPDATE m_users SET login_fail_date= null, login_fail_cut= 0, lock_date= null, last_login_date=?, lock_flg= 0 WHERE user_id=?');
+            $login->execute(array(date("Ymd"),$member['user_id']));
+
+            // SESSIONにユーザーID保持
+            $_SESSION['login']['user_id'] = $member['user_id'];
+            $_SESSION['login']['user_name'] = $member['user_name'];
+            $_SESSION['login']['auth'] = $member['auth'];
+
+            // エラー内容の初期化
+            $error['login'] = '';
+            header('Location: menu.php');
+            exit();
+        } elseif ($member['lock_flg'] === '1' || $member['lock_flg'] === '9') {
+            // ＊ロック中
+            $error['login'] = '* ロック中です。<br>あと'. (10 - $difference) . '分でロックが解除されます。';
+        } else {
+            // ＊パスワードが一致しない時
+            $logmiss = $member['login_fail_cut'] + 1;
+
+            if ($logmiss === 3) {
+                // ３回ログインミスした時
+                $penalty = $db->prepare('UPDATE m_users SET lock_date= ?, lock_flg=?, login_fail_cut=?, login_fail_date=? WHERE email=?');
+                $penalty->execute(array($misstime,1,$logmiss,$misstime,$_POST['email']));
+                $error['login'] = '* ログインに規定回数以上失敗したため、ロックしました。時間をおいて再ログインしてください。';
+
+            } else {
+                // ＊3回未満のログインミス
+                $penalty = $db->prepare('UPDATE m_users SET login_fail_cut=?, login_fail_date=? WHERE email=?');
+                $penalty->execute(array($logmiss,$misstime,$_POST['email']));
+            }
+        }
     } else {
         // メールアドレスとパスワードの片方でも未入力
         if ($_POST['email'] === '') {
@@ -87,13 +106,9 @@ unset($_SESSION['login']);
         body {
             height: 100;
             text-align: center;
-            margin-top: 20px;
+            margin-top: 3%;
         }
 
-        table {
-            width: 80%;
-            height: 40%;
-        }
     </style>
 </head>
 <body>
@@ -113,15 +128,15 @@ unset($_SESSION['login']);
         <?php endif; ?>
         </h3>
 
-    <form method="post" style="margin-top: 10%;">
-        <div>
+    <form method="post" style="margin-top: 7%;">
+        <div style="font-size: 24px">
             <!-- メールアドレス入力 -->
             <strong>メールアドレス　　　</strong>
-            <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
+            <input type="text" name="email" size="40" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
             <br><br>
             <!-- パスワード入力 -->
             <strong>パスワード　　　　　</strong>
-            <input type="password" name="password" size="35" maxlength="16" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
+            <input type="password" name="password" size="40" maxlength="16" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
         </div>
 
         <br><br>
@@ -132,13 +147,6 @@ unset($_SESSION['login']);
         </div>
     </form>
     </div>
-
-    <!-- 確認用（新規画面＆パスワードの暗号化コード完成まで付加） -->
-    <form action="menu.php">
-        <div>
-            <input type="submit" value="menu" style="margin-left: 70%; margin-top: 10%; float:left; width: 300px;">
-        </div>
-    </form>
 
 </body>
 </html>
