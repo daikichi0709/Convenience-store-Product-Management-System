8db19a38f59bec0f0c2e7a9ece70705ebde158e8
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-21 11:32:20 +0900

    関連PHPファイル名変更

--- menu.php
+++ menu.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
 // DB接続
-require('dbconnect.php');
+require('Common.php');
 
 
-    $keyid = $_SESSION['login']['user_id'];
-    $login_userdata = $db->prepare('SELECT user_name, auth FROM m_users WHERE user_id=?');
-    $login_userdata->execute(array($keyid));
-    $member = $login_userdata->fetch();
+$keyid = $_SESSION['login']['user_id'];
+$login_userdata = $db->prepare('SELECT user_name, auth FROM m_users WHERE user_id=?');
+$login_userdata->execute(array($keyid));
+$member = $login_userdata->fetch();
 
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta>
     <meta charset="UTF-8">
@@ -35,35 +36,37 @@ require('dbconnect.php');
         }
     </style>
 </head>
+
 <body>
     <div>
         <h1>商品管理システム</h1>
-        <p style="font-size: 24px;">ログインユーザー名：<strong><?php print($member['user_name']." auth= \n".$member['auth']) ?></strong></p>
+        <p style="font-size: 24px;">ログインユーザー名：<strong><?php print($member['user_name']) ?></strong></p>
         <br>
         <div style="display:inline-flex">
             <!-- 在庫管理一覧へ画面遷移 -->
             <form action="stock.php">
                 <div>
-                <input type="submit" value="在庫管理" style="font-size: 30px; margin:50px; float:left; height: 100px; width: 300px; background-color:#ffff99;" onmouseover="this.style.background='#99ccff'" onmouseout="this.style.background='#ffff99';">
+                    <input type="submit" value="在庫管理" style="font-size: 30px; margin:50px; float:left; height: 100px; width: 300px; background-color:#ffff99;" onmouseover="this.style.background='#99ccff'" onmouseout="this.style.background='#ffff99';">
                 </div>
             </form>
 
-            <?php if($member['auth'] === "1") :?>
-            <!-- ユーザー管理一覧へ画面遷移 -->
-            <form action="users.php">
-                <div>
-                <input type="submit" value="ユーザー管理" style="font-size: 30px; margin:50px; float:left; height: 100px; width: 300px; background-color:#ffff99;" onmouseover="this.style.background='#99ccff'" onmouseout="this.style.background='#ffff99';">
-                </div>
-            </form>
+            <?php if ($member['auth'] === "1") : ?>
+                <!-- ユーザー管理一覧へ画面遷移 -->
+                <form action="users.php">
+                    <div>
+                        <input type="submit" value="ユーザー管理" style="font-size: 30px; margin:50px; float:left; height: 100px; width: 300px; background-color:#ffff99;" onmouseover="this.style.background='#99ccff'" onmouseout="this.style.background='#ffff99';">
+                    </div>
+                </form>
             <?php endif; ?>
         </div>
         <br><br>
         <form action="login.php">
             <div>
-            <input type="submit" value="ログアウト" style="font-size: 20px; height: 80px;width: 200px;">
+                <input type="submit" value="ログアウト" style="font-size: 20px; height: 80px;width: 200px;">
             </div>
         </form>
     </div>
 
 </body>
+
 </html>
