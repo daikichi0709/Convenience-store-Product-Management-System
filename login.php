99222f58aa003c8035a6ac6a2bec5ef60562600b
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-21 09:24:51 +0900

    再コミット

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if (!empty($_POST)) {
     $error['login'] = 'ログインに失敗しました';
 
     $email = $_POST['email'];
-    // ＊メアドとパスワードが共に入力されているか
+    // *メアドとパスワードが共に入力されているか
     if ($_POST['email'] !== '' && $_POST['password'] !== '') {
         $login = $db->prepare('SELECT user_id, email, password, login_fail_date, lock_flg, login_fail_cut FROM m_users WHERE email=?');
         $login->execute(array($_POST['email']));
@@ -36,16 +36,16 @@ if (!empty($_POST)) {
             // １０分以上経過 ＆ 管理者からのロックではない時、ならばロックカウント、ロックフラグの初期化
             if ($difference >= "10" and $member['lock_flg'] !== "9") {
                 $login = $db->prepare('UPDATE m_users SET login_fail_date= null, login_fail_cut= 0, lock_date= null, lock_flg= 0, upd_date=?, upd_user_id=? WHERE user_id=?');
-                $login->execute(array($datetime,$member['user_id'],$member['user_id']));
+                $login->execute(array($datetime, $member['user_id'], $member['user_id']));
             }
         }
 
 
-        // ＊保存パスワード　＝　入力パスワード かつ　ロック中ではない時
+        // *保存パスワード ＝ 入力パスワード かつ ロック中ではない時
         if (password_verify($_POST['password'], $member['password']) && $member['lock_flg'] === '0') {
             //ログイン成功：失敗項目の初期化
             $login = $db->prepare('UPDATE m_users SET login_fail_date= null, login_fail_cut= 0, lock_date= null, last_login_date=?, lock_flg= 0, upd_date=?,  upd_user_id=? WHERE user_id=?');
-            $login->execute(array($datetime,$datetime,$member['user_id'],$member['user_id']));
+            $login->execute(array($datetime, $datetime, $member['user_id'], $member['user_id']));
 
             // SESSIONにユーザーID保持
             $_SESSION['login']['user_id'] = $member['user_id'];
@@ -55,33 +55,32 @@ if (!empty($_POST)) {
             header('Location: menu.php');
             exit();
         } elseif ($member['lock_flg'] === '1' || $member['lock_flg'] === '9') {
-            // ＊ロック中
+            // *ロック中
             $error['login'] = 'ロック中です';
         } else {
-            // ＊パスワードが一致しない時
+            // *パスワードが一致しない時
             $logmiss = $member['login_fail_cut'] + 1;
 
             if ($logmiss === 3) {
-                // ３回ログインミスした時
+                // *３回ログインミスした時
                 $penalty = $db->prepare('UPDATE m_users SET lock_date= ?, lock_flg=?, login_fail_cut=?, login_fail_date=?, upd_date=?, upd_user_id=? WHERE email=?');
-                $penalty->execute(array($datetime,1,$logmiss,$datetime,$datetime,$member['user_id'],$_POST['email']));
+                $penalty->execute(array($datetime, 1, $logmiss, $datetime, $datetime, $member['user_id'], $_POST['email']));
                 $error['login'] = 'ログインに規定回数以上失敗したため、ロックしました。時間をおいて再ログインしてください';
-
             } else {
-                // ＊３回未満のログインミス
+                // *３回未満のログインミス
                 $penalty = $db->prepare('UPDATE m_users SET login_fail_cut=?, login_fail_date=?, upd_date=?, upd_user_id=? WHERE email=?');
-                $penalty->execute(array($logmiss,$datetime,$datetime,$member['user_id'],$_POST['email']));
+                $penalty->execute(array($logmiss, $datetime, $datetime, $member['user_id'], $_POST['email']));
             }
         }
     } else {
         // メールアドレスとパスワードの片方でも未入力
         if ($_POST['email'] === '') {
             // エラーメアド
-            $error['message'] .= '<br> メールアドレスが未入力です。';
+            $error['message'] .= '<br> メールアドレスが未入力です';
         }
         if ($_POST['password'] === '') {
             // エラーパスワード
-            $error['message'] .= '<br> パスワードが未入力です。';
+            $error['message'] .= '<br> パスワードが未入力です';
         }
     }
 }
@@ -90,6 +89,7 @@ session_destroy();
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta>
     <meta charset="UTF-8">
@@ -104,45 +104,46 @@ session_destroy();
             text-align: center;
             margin-top: 3%;
         }
-
     </style>
 </head>
+
 <body>
     <div>
         <a href="login.php">
-        <h1>商品管理システム</h1>
+            <h1>商品管理システム</h1>
         </a>
 
         <h3 style="color: red;">
-        <!-- ログイン失敗 -->
-        <?php
-        if ($error['login'] !== ''):
-            print($error['login']);
-            if (!empty($error['message'])):
-                print($error['message']);
-            endif; ?>
-        <?php endif; ?>
+            <!-- ログイン失敗 -->
+            <?php
+            if ($error['login'] !== '') :
+                print($error['login']);
+                if (!empty($error['message'])) :
+                    print($error['message']);
+                endif; ?>
+            <?php endif; ?>
         </h3>
 
-    <form method="post" style="margin-top: 7%;">
-        <div style="font-size: 24px">
-            <!-- メールアドレス入力 -->
-            <strong>メールアドレス　　　</strong>
-            <input type="text" name="email" size="40" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
+        <form method="post" style="margin-top: 7%;">
+            <div style="font-size: 24px">
+                <!-- メールアドレス入力 -->
+                <strong>メールアドレス　　　</strong>
+                <input type="text" name="email" size="40" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
+                <br><br>
+                <!-- パスワード入力 -->
+                <strong>パスワード　　　　　</strong>
+                <input type="password" name="password" size="40" maxlength="16" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
+            </div>
+
             <br><br>
-            <!-- パスワード入力 -->
-            <strong>パスワード　　　　　</strong>
-            <input type="password" name="password" size="40" maxlength="16" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
-        </div>
 
-        <br><br>
-
-        <div>
-            <!-- ログインボタン -->
-            <input type="submit" value="ログイン" style="width: 200px;">
-        </div>
-    </form>
+            <div>
+                <!-- ログインボタン -->
+                <input type="submit" value="ログイン" style="width: 200px;">
+            </div>
+        </form>
     </div>
 
 </body>
+
 </html>
