34712f0685f15a12f0a3fd5211e737f5cefeb337
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-21 09:59:50 +0900

    WIP:80%完成したので確認お願いします。

--- stock.php
+++ stock.php
@@ -6,8 +6,8 @@ require('dbconnect.php');
 
 $page = $_REQUEST['page'];
 
-if($page == ''){
-  $page = 1;
+if ($page == '') {
+    $page = 1;
 }
 $page = max($page, 1);
 
@@ -15,13 +15,13 @@ $itemcnt = $db->query('SELECT COUNT(*) AS cnt FROM t_inventories');
 $cnt = $itemcnt->fetch();
 $maxPage = ceil($cnt['cnt'] / 10); //ceil():切り上げ
 
-$page = min($page,$maxPage);
+$page = min($page, $maxPage);
 
 //出力ページで出す項目の一番上の番号の計算式
 $start = ($page - 1) * 10;
 
 $items = $db->prepare('SELECT item_id, item_name, price, stock, day FROM t_inventories ORDER BY item_id ASC LIMIT ?,10');
-$items->bindParam(1,$start,PDO::PARAM_INT);
+$items->bindParam(1, $start, PDO::PARAM_INT);
 $items->execute();
 
 $keyid = $_SESSION['login']['user_id'];
@@ -33,6 +33,7 @@ $auth = $login_userdata->fetch();
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta>
     <meta charset="UTF-8">
@@ -54,78 +55,95 @@ $auth = $login_userdata->fetch();
         }
     </style>
 </head>
+
 <body>
     <div>
 
-    <a>
-    <h1>在庫一覧</h1>
-    </a>
+        <a>
+            <h1>在庫一覧</h1>
+        </a>
 
-    <p style="font-size: 20px; margin-right: 10%; text-align: right;">
-    <a href="ins_product.php">新規追加</a>
-    <a href="">CSVダウンロード</a>
-    </p>
+        <?php if ($auth['auth'] === "1" || $auth['auth'] === "2") : ?>
+            <p style="font-size: 20px; margin-right: 10%; text-align: right;">
+                <a href="ins_product.php">新規追加</a>
+                <a href="">CSVダウンロード</a>
+            </p><?php endif; ?>
 
 
-    <center><table border="3" style="font-size: 24px;">
-    <!-- 1行目：項目名 -->
-    <tr>
-    <th>NO.</th>
-    <th>商品名</th>
-    <th>価格</th>
-    <th>在庫数</th>
-    <th>入荷日</th>
-    <th>　</th>
-    </tr>
+        <center>
+            <table border="3" style="font-size: 24px;">
+                <!-- 1行目：項目名 -->
+                <tr>
+                    <th>NO.</th>
+                    <th>商品名</th>
+                    <th>価格</th>
+                    <th>在庫数</th>
+                    <th>入荷日</th>
+                    <!-- 権限が「1」「2」のユーザーのみの仕様 -->
+                    <?php if ($auth['auth'] === "1" || $auth['auth'] === "2") : ?>
+                        <th>　</th>
+                    <?php endif; ?>
+                </tr>
 
-    <!-- 2行目以降：項目 -->
-    <!-- <php for($i = 1;$i <= 10;$i++): ?> -->
-    <?php while ($item = $items->fetch()): ?>
-    <tr>
-    <td><?php print($item['item_id']) ?></td>
-    <td><?php print($item['item_name']) ?></td>
-    <td><?php print($item['price']) ?></td>
-    <td><?php print($item['stock']) ?></td>
-    <td><?php print($item['day']) ?></td>
+                <!-- 2行目以降：項目 -->
+                <?php while ($item = $items->fetch()) : ?>
+                    <tr>
+                        <td><?php print($item['item_id']) ?></td>
+                        <td><?php print($item['item_name']) ?></td>
+                        <td><?php print($item['price']) ?></td>
+                        <td><?php print($item['stock']) ?></td>
+                        <td><?php print($item['day']) ?></td>
 
-    <th><?php if($auth['auth'] === "1" || $auth['auth'] ==="2"): ?>
-    <a href="upd_product.php">編集</a>|<a href="del_product.php">削除</a></th>
-    <?php endif; ?></tr>
-    <!-- <php endfor; ?> -->
-    <?php endwhile; ?>
+                        <!-- 権限が「1」「2」のユーザーのみの仕様 -->
+                        <?php if ($auth['auth'] === "1" || $auth['auth'] === "2") : ?>
+                            <th>
+                                <a href="upd_product.php?item_id=<?php print($item['item_id']); ?>">編集</a>
+                                |<a href="">削除</a>
+                            </th>
+                        <?php endif; ?>
+                    </tr>
 
-    </p>
+                <?php endwhile; ?>
+
+                </p>
     </div>
 
-    </table></center>
+    </table>
+    </center>
 
     <br>
 
     <p style="margin-right: 20%; text-align: right;">
 
-    <?php if($page > 1): ?>
-    <a href="stock.php?page=1">《</a>
-    <?php else: ?>《<?php endif; ?>
+        <?php if ($page > 1) : ?>
+            <a href="stock.php?page=1">《</a>
+            <?php else : ?>《
+        <?php endif; ?>
 
-    <?php if($page > 1): ?>
-    <a href="stock.php?page=<?php print($page-1); ?>">〈</a>
-    <?php else: ?>〈<?php endif; ?>
+        <?php if ($page > 1) : ?>
+            <a href="stock.php?page=<?php print($page - 1); ?>">〈</a>
+            <?php else : ?>〈
+        <?php endif; ?>
 
-    <a href="">1<?php  ?></a> <!-- 数字は変数に変えます。 -->
-    <a href="">2<?php  ?></a> <!-- 数字は変数に変えます。 -->
-    <a href="">3<?php  ?></a> <!-- 数字は変数に変えます。 -->
-    <a href="">4<?php  ?></a> <!-- 数字は変数に変えます。 -->
+        <?php for ($pagecut = 1; $pagecut <= $maxPage; $pagecut++) : ?>
+            <a href="stock.php?page=<?php print($pagecut); ?>"><?php print($pagecut); ?></a>
+        <?php endfor; ?>
 
-    <?php if($page < $maxPage): ?>
-    <a href="stock.php?page=<?php print($page+1); ?>">〉</a>
-    <?php else: ?>〉<?php endif; ?>
+        <?php if ($page < $maxPage) : ?>
+            <a href="stock.php?page=<?php print($page + 1); ?>">〉</a>
+            <?php else : ?>〉
+        <?php endif; ?>
 
-    <?php if($page < $maxPage): ?>
-    <a href="stock.php?page=<?php print($maxPage); ?>">》</a>
-    <?php else: ?>》<?php endif; ?>
+        <?php if ($page < $maxPage) : ?>
+            <a href="stock.php?page=<?php print($maxPage); ?>">》</a>
+            <?php else : ?>》
+        <?php endif; ?>
     </p>
 
     </div>
-    <a href="menu.php"><p style="margin-left: 10%; text-align: left;">≪ 戻る</a>
+    <a href="menu.php">
+        <p style="margin-left: 10%; text-align: left;">≪ 戻る
+    </a>
 </body>
+
 </html>
