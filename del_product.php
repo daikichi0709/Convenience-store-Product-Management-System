177b251b84a78160acb0d5386ded61641fafa66e
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-27 11:44:07 +0900

    Ajaxでの処理

--- del_product.php
+++ del_product.php
@@ -3,19 +3,30 @@ session_start();
 // DB接続
 require('Common.php');
 
-if (isset($_REQUEST['item_id']) && is_numeric($_REQUEST['item_id'])) {
-    $id = $_REQUEST['item_id'];
+// JSONにする
+header('Content-Type：application/json; charset=UTF-8');
 
-    $today = new DateTime();
-    $deltime = $today->format('Y-m-d H:i:s');
+//仮にPOSTの値が何もなければ不正リクエストとして返金
+if(empty($_POST)){
+    $response[] = ['error' => '不正リクエストです','status' => 405];
+    echo  json_encode($response,JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    exit();
+}
 
-    $items = $db->prepare('UPDATE t_inventories SET del_flg=1, upd_date=?, upd_user_id=? WHERE item_id=?');
-    $items->execute(array($deltime, $_SESSION['login']['user_id'], $id));
-    header('Location: stock.php');
-} else {
-    ?>
-    <!-- PHPでアラートを表示する -->
-    <script>
-        alert('データ不整合です')
-    </script>
-<?php }
+$id = $_POST['item_id'];
+
+$today = new DateTime();
+$deltime = $today->format('Y-m-d H:i:s');
+
+$items = $db->prepare('UPDATE t_inventories SET del_flg=1, upd_date=?, upd_user_id=? WHERE item_id=?');
+$items->execute(array($deltime, $_SESSION['login']['user_id'], $id));
+
+$response  = [];
+
+// もらった値をそのままかえす
+$response[] = ['id' => $_POST['item_id']];
+echo  json_encode($response,JSON_UNESCAPED_SLASHES  | JSON_UNESCAPED_UNICODE);
+
+
+exit();
+?>
