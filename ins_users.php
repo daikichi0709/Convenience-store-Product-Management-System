2c3e98a54682777534c73c65dc6ce44bc2c4a541
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-28 14:18:03 +0900

    InReview：ユーザー登録

--- ins_users.php
+++ ins_users.php
@@ -1,42 +1,33 @@
 <?php
 session_start();
 // DB接続
-try {
-    $db = new PDO('mysql:dbname=conveni_db;host=127.0.0.1; charset=utf8', 'root', '');
-} catch (PDOException $e) {
-    print('DB接続エラー：' . $e->getMessage());
-    exit();
-}
+require('Common.php');
 
-if(!empty($_POST)){
+if (!empty($_POST)) {
     $_SESSION['join'] = $_POST;
-    if($_SESSION['join']['password1'] === $_SESSION['join']['password2']) {
+    if ($_SESSION['join']['password1'] === $_SESSION['join']['password2']) {
+
         $statement = $db->prepare('INSERT INTO m_users SET user_name=?, email=?, password=?, auth=?, ins_user_id=?, upd_user_id=?, ins_date=?, upd_date=?, last_login_date=?, login_fail_cut=0, del_flg=0, lock_flg=0');
 
-            $statement->execute(array(
-            $_SESSION['join']['user_name'],
-            $_SESSION['join']['email'],
-            sha1($_SESSION['join']['password1']),
-            $_SESSION['join']['auth'],
-            $_SESSION['login']['user_id'],
-            $_SESSION['login']['user_id'],
-            time(),
-            time(),
-            time()
-        ));
-        $kari = $_SESSION['join'];
+        //パスワードの暗号化
+        $hash_pass = password_hash($_SESSION['join']['password1'], PASSWORD_DEFAULT);
+        //日時設定
+        $today = new DateTime();
+        $instime = $today->format('Y-m-d H:i:s');
+
+        $statement->execute(array($_SESSION['join']['user_name'], $_SESSION['join']['email'], $hash_pass, $_SESSION['join']['auth'], $_SESSION['login']['user_id'], $_SESSION['login']['user_id'], $instime, $instime, $instime));
+
         unset($_SESSION['join']);
-        if(empty($_SESSION['join'])){
-            header('Location: menu.php');
+        if (empty($_SESSION['join'])) {
+            header('Location: users.php');
             exit();
         }
-
     }
-
 }
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta>
     <meta charset="UTF-8">
@@ -58,52 +49,56 @@ if(!empty($_POST)){
         }
     </style>
 </head>
+
 <body>
     <div>
-    <h1>ユーザー登録</h1>
+        <h1>ユーザー登録</h1>
 
-    <!-- エラーメッセージ -->
-    <h3>
-    <?php if ($error[''] === ''): ?>
-    <p>* </p>
-    <?php endif; ?>
-    </h3>
+        <!-- エラーメッセージ -->
+        <h3>
+            <?php if ($error[''] === '') : ?>
+                <p>* </p>
+            <?php endif; ?>
+        </h3>
 
-    <form action="" method="post">
-        <div>
-            <!-- 商品名 -->
-            <strong style="width: 200px;">ユーザー名　　　　　　</strong>
-            <textarea placeholder="ユーザー名を入力してください" name="user_name"" rows="1" cols="100" maxlength="255" ><?php print(htmlspecialchars($_SESSION['join']['user_name'], ENT_QUOTES)); ?></textarea>
+        <form action="" method="post">
+            <div style="font-size: 24px">
+                <!-- 商品名 -->
+                <strong style="width: 200px;">ユーザー名　　　　　　</strong>
+                <input type="text" placeholder="ユーザー名を入力してください" name="user_name" style="font-size: 18px; width: 500px" maxlength="255"><?php print(htmlspecialchars($_SESSION['join']['user_name'], ENT_QUOTES)); ?>
 
+                <br><br>
+                <!-- 商品説明 -->
+                <strong style="width: 200px;">メールアドレス　　　　</strong>
+                <input type="text" placeholder="メールアドレスを入力してください" name="email" style="font-size: 18px; width: 500px" maxlength="255"><?php print(htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES)); ?>
+
+                <br><br>
+                <!-- 仕入先 -->
+                <strong style="width: 200px;">パスワード　　　　　　</strong>
+                <input type="text" placeholder="パスワードを設定してください" name="password1" style="font-size: 18px; width: 500px" maxlength="255"><?php print(htmlspecialchars($_SESSION['join']['password1'], ENT_QUOTES)); ?>
+
+                <br><br>
+                <!-- 生産国 -->
+                <strong style="width: 200px;">パスワード（確認）　　</strong>
+                <input type="text" placeholder="確認のため設定したパスワードを入力してください" name="password2" style="font-size: 18px; width: 500px" maxlength="255"><?php print(htmlspecialchars($_SESSION['join']['password2'], ENT_QUOTES)); ?>
+
+                <br><br>
+                <!-- 価格 -->
+                <strong style="width: 200px;">権限　　　　　　　　　</strong>
+                <input type="text" placeholder="権限を数値で決めて下さい【1:管理者,2:発注担当者,3:閲覧者】" name="auth" style="font-size: 18px; width: 500px" maxlength="255"><?php print(htmlspecialchars($_SESSION['join']['auth'], ENT_QUOTES)); ?>
+
+            </div>
             <br><br>
-            <!-- 商品説明 -->
-            <strong style="width: 200px;">メールアドレス　　　　</strong>
-            <textarea placeholder="メールアドレスを入力してください" name="email" rows="1" cols="100" maxlength="255" ><?php print(htmlspecialchars($_SESSION['join']['email'], ENT_QUOTES)); ?></textarea>
-
-            <br><br>
-            <!-- 仕入先 -->
-            <strong style="width: 200px;">パスワード　　　　　　</strong>
-            <textarea placeholder="パスワードを設定してください" name="password1" rows="1" cols="100" maxlength="255" ><?php print(htmlspecialchars($_SESSION['join']['password1'], ENT_QUOTES)); ?></textarea>
-
-            <br><br>
-            <!-- 生産国 -->
-            <strong style="width: 200px;">パスワード（確認）　　</strong>
-            <textarea placeholder="確認のため設定したパスワードを入力してください" name="password2" rows="1" cols="100" maxlength="255" ><?php print(htmlspecialchars($_SESSION['join']['password2'], ENT_QUOTES)); ?></textarea>
-
-            <br><br>
-            <!-- 価格 -->
-            <strong style="width: 200px;">権限　　　　　　　　　</strong>
-            <textarea placeholder="権限を決めて下さい【1:管理者,2:発注担当者,3:閲覧者】" name="auth" rows="1" cols="100" maxlength="255" ><?php print(htmlspecialchars($_SESSION['join']['auth'], ENT_QUOTES)); ?></textarea>
-
-        </div>
-        <br><br>
-        <div>
-            <!-- 登録ボタン -->
-            <input type="submit" value="登録" style="font-size: 30px; width: 150px; height: 50px;">
-        </div>
-    </form>
+            <div>
+                <!-- 登録ボタン -->
+                <input type="submit" value="登録" style="font-size: 30px; width: 150px; height: 50px;">
+            </div>
+        </form>
     </div>
 
-    <a href="menu.php"><p style="margin-left: 20%; text-align: left;">≪ 戻る</a>
+    <a href="users.php">
+        <p style="margin-left: 20%; text-align: left;">≪ 戻る
+    </a>
 </body>
+
 </html>
