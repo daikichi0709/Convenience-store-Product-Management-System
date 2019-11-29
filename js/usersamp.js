6dbf795f0ffaa20ff7220c8cc46cd4e07c6d1438
daikichi0709 <daiki_ito@3ca.co.jp>
2019-11-29 10:37:00 +0900

    ユーザー側の.jsファイルの統合

--- js/usersamp.js
+++ js/usersamp.js
@@ -4,7 +4,6 @@ $(function () {
         ajaxCall1(this);
     });
 
-
     function ajaxCall1(target) {
         $.ajax({
             url: 'del_users.php',
@@ -21,6 +20,30 @@ $(function () {
             // コール失敗の動作
             console.log(jqXHR, status, error);  // TEST
             alert('削除できませんでした');
+        });
+    }
+
+    $('.push2').click(function () {
+        // 関数呼び出し
+        ajaxCall2(this);
+    });
+
+    function ajaxCall2(target) {
+        $.ajax({
+            url: 'lockswitch.php',
+            dateType: 'json',
+            type: 'POST',
+            data: {
+                'user_id': $(target).prev('.user_id').val()
+            }
+        }).done(function (data, status, jqXHR) {
+            // コール成功の動作
+            console.log(data, status, jqXHR);  // TEST
+            alert('ロック切り換えしました');
+        }).fail(function (jqXHR, status, error) {
+            // コール失敗の動作
+            console.log(jqXHR, status, error);  // TEST
+            alert('ロック切り換えできませんでした');
         });
     }
 });
