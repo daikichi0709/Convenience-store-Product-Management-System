9231b6503a0f12cc2edb641b043a9541913c030b
daikichi0709 <daiki_ito@3ca.co.jp>
2019-12-03 10:08:00 +0900

    レビュー訂正 ＄＿REQUESTを＄＿POSTへ変換 権限のよる制御

--- upd_users.php
+++ upd_users.php
@@ -3,8 +3,8 @@ session_start();
 // DB接続
 require('Common.php');
 
-if (isset($_REQUEST['user_id']) && is_numeric($_REQUEST['user_id'])) {
-    $id = $_REQUEST['user_id'];
+if (isset($_POST['user_id']) && is_numeric($_POST['user_id'])) {
+    $id = $_POST['user_id'];
 
     $users = $db->prepare('SELECT user_id, user_name, email, password, auth FROM m_users WHERE user_id=?');
     $users->execute(array($id));
@@ -24,63 +24,63 @@ if (isset($_REQUEST['user_id']) && is_numeric($_REQUEST['user_id'])) {
 }
 
 $errormessage  = '';
-$udeuser = [];
+$upduser = [];
 
 if (!empty($_POST)) {
-    $udeuser = $_POST;
+    $upduser = $_POST;
 
     // ユーザー名
-    if (empty($udeuser['user_name'])) {
+    if (empty($upduser['user_name'])) {
         $errormessage .= "ユーザー名が未入力です" . "<br>";
     } else {
-        if (mb_strlen($udeuser['user_name']) > 20) {
+        if (mb_strlen($upduser['user_name']) > 20) {
             $errormessage .= "ユーザー名に入力できる文字数を超えています" . "<br>";
         }
     }
 
     // メールアドレス
-    if (empty($udeuser['email'])) {
+    if (empty($upduser['email'])) {
         $errormessage .= "メールアドレスが未入力です" . "<br>";
     } else {
-        if (mb_strlen($udeuser['email']) > 255) {
+        if (mb_strlen($upduser['email']) > 255) {
             $errormessage .= "メールアドレスに入力できる文字数を超えています" . "<br>";
         }
     }
 
     // パスワード
-    if (empty($udeuser['password'])) {
+    if (empty($upduser['password'])) {
         $errormessage .= "パスワードが未入力です" . "<br>";
     } else {
-        if (mb_strlen($udeuser['password']) < 6 || mb_strlen($udeuser['password']) >= 16) {
+        if (mb_strlen($upduser['password']) < 6 || mb_strlen($upduser['password']) >= 16) {
             $errormessage .= "パスワードが6文字以上16文字未満にしてください" . "<br>";
         }
-        if ($udeuser['password'] !== $udeuser['protpassword']) {
+        if ($upduser['password'] !== $upduser['protpassword']) {
             $errormessage .= "パスワードが一致しません<br>";
         }
     }
 
-    // 権限{{
+    // 権限
     if ($authcontrol !== 1) {
-        if (empty($udeuser['auth'])) {
+        if (empty($upduser['auth'])) {
             $errormessage .= "権限が未設定です<br>";
-        } elseif ($udeuser['auth'] !== "1" || $udeuser['auth'] !== "2" || $udeuser['auth'] !== "3") {
+        } elseif ($upduser['auth'] !== '1' && $upduser['auth'] !== '2' && $upduser['auth'] !== '3') {
             $errormessage .= "権限が設定外です<br>";
         }
     }
 
-
     // ユーザー編集処理
     if (empty($errormessage)) {
-        $statement = $db->prepare('UPDATE m_users SET user_name=?, email=?, password=?, auth=?, upd_date=?, upd_user_id=? WHERE user_id=?');
 
         //パスワードの暗号化
-        $hash_pass = password_hash($udeuser['password'], PASSWORD_DEFAULT);
+        $hash_pass = password_hash($upduser['password'], PASSWORD_DEFAULT);
 
         //日時設定
         $today = new DateTime();
-        $instime = $today->format('Y-m-d H:i:s');
+        $updtime = $today->format('Y-m-d H:i:s');
 
-        $statement->execute(array($udeuser['user_name'], $udeuser['email'], $hash_pass,  $udeuser['auth'],  $instime,  $_SESSION['login']['user_id'], $id));
+        $statement = $db->prepare('UPDATE m_users SET user_name=?, email=?, password=?, auth=?, upd_date=?, upd_user_id=? WHERE user_id=?');
+
+        $statement->execute(array($upduser['user_name'], $upduser['email'], $hash_pass, $upduser['auth'],  $updtime,  $_SESSION['login']['user_id'], $user['user_id']));
 
         $_SESSION['result'] = 1; //更新完了フラグ
         header('Location: users.php');
@@ -88,6 +88,7 @@ if (!empty($_POST)) {
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -100,11 +101,19 @@ if (!empty($_POST)) {
     <title>商品管理システム【ユーザー更新画面】</title>
     <!-- <link rel="stylesheet" href="style.css"> -->
     <style>
-        html,
         body {
             height: 100;
             text-align: center;
-            margin-top: 20px;
+            margin-top: 3%;
+            /* background-image: url("レトロ.jpg"); */
+        }
+
+        h3 {
+            color: red;
+        }
+
+        strong {
+            background-color: #blue;
         }
 
         table {
@@ -124,10 +133,11 @@ if (!empty($_POST)) {
         <?php endif; ?>
 
         <form method="post">
+            <input type="hidden" name="user_id" value="<?php print(htmlspecialchars($user['user_id'], ENT_QUOTES)); ?>">
             <div style="font-size: 24px">
                 <!-- ユーザー名 -->
                 <strong style="width: 200px;">ユーザー名　　　　　　</strong>
-                <input type="text" placeholder="ユーザー名を入力してください" name="user_name" maxlength="255" style="font-size: 18px; width: 500px;" value="<?php if (empty($_POST)) {
+                <input type="text" placeholder="ユーザー名を入力してください" name="user_name" maxlength="255" style="font-size: 18px; width: 500px;" value="<?php if (empty($_POST['user_name'])) {
                                                                                                                                                     print(htmlspecialchars($user['user_name'], ENT_QUOTES));
                                                                                                                                                 } else {
                                                                                                                                                     print(htmlspecialchars($_POST['user_name'], ENT_QUOTES));
@@ -137,7 +147,7 @@ if (!empty($_POST)) {
                 <br><br>
                 <!-- メールアドレス -->
                 <strong style="width: 200px;">メールアドレス　　　　</strong>
-                <input type="text" placeholder="メールアドレスを入力してください" name="email" maxlength="255" style="font-size: 18px; width: 500px;" value="<?php if (empty($_POST)) {
+                <input type="text" placeholder="メールアドレスを入力してください" name="email" maxlength="255" style="font-size: 18px; width: 500px;" value="<?php if (empty($_POST['email'])) {
                                                                                                                                                     print(htmlspecialchars($user['email'], ENT_QUOTES));
                                                                                                                                                 } else {
                                                                                                                                                     print(htmlspecialchars($_POST['email'], ENT_QUOTES));
@@ -160,10 +170,12 @@ if (!empty($_POST)) {
                     <strong style="width: 200px;">権限　　　　　　　　　</strong>
                     <select name="auth" style="font-size: 18px; width: 500px;">
                         <option value="">選択</option>
-                        <option value="1">管理者</option>
-                        <option value="2">発注担当者</option>
-                        <option value="3">閲覧者</option>
+                        <option value=1>管理者</option>
+                        <option value=2>発注担当者</option>
+                        <option value=3>閲覧者</option>
                     </select>
+                <?php elseif ($authcontrol === 1) : ?>
+                    <input type="hidden" name="auth" value=1 style="font-size: 18px; width: 500px;">
                 <?php endif; ?>
             </div>
 
