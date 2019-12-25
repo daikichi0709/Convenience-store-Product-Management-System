99f0ce1dacd5fd0eded1d966acbf941f8aa8c97e
daikichi0709 <daiki_ito@3ca.co.jp>
2019-12-25 13:57:37 +0900

    仕入れ価格の範囲設定での変数名を修正

--- ins_product.php
+++ ins_product.php
@@ -3,108 +3,119 @@ session_start();
 // DB接続
 require('Common.php');
 
+$keyid = $_SESSION['login']['user_id'];
+$login_userdata = $db->prepare('SELECT auth FROM m_users WHERE user_id=?');
+$login_userdata->execute(array($keyid));
+$auth = $login_userdata->fetch();
+
+if ($auth['auth'] !== "1") {
+    header('Location: error.php');
+    exit();
+}
+
+
+if (isset($_GET['user_id']) && is_numeric($_GET['user_id'])) {
+    $id = $_GET['user_id'];
+    $users = $db->prepare('SELECT user_id, user_name, email, password, auth FROM m_users WHERE user_id=?');
+    $users->execute(array($id));
+    $user = $users->fetch();
+
+    if (empty($user)) {
+        $_SESSION['result'] = 2; //ユーザー不在フラグ
+        header('Location: users.php');
+        exit();
+    }
+}
+//権限制御(変更できる権限のユーザーをオンにする)
+$authcontrol = "";
+if ($user['user_id'] === $_SESSION['login']['user_id'] || $_POST['userid'] === $_SESSION['login']['user_id']) {
+    $authcontrol = "X";
+}
+
+// 権限マスタより権限情報を取得
+$auths = $db->query('SELECT auth, auth_name FROM m_auth');
+
 $errormessage  = '';
-$itemarray = [];
+$errorauth  = '';
+$upduser = [];
+
 if (!empty($_POST)) {
-    $itemarray = $_POST;
-
-    // 商品名
-    if (empty($itemarray['item_name'])) {
-        $errormessage .= "商品名が未入力です" . "<br />";
+    $upduser = $_POST;
+    // ユーザー名
+    if (empty($upduser['user_name'])) {
+        $errormessage .= "ユーザー名が未入力です" . "<br>";
     } else {
-        if (mb_strlen($itemarray['item_name']) > 30) {
-            $errormessage .= "商品名に入力できる文字数を超えています" . "<br>";
+        if (mb_strlen($upduser['user_name']) > 20) {
+            $errormessage .= "ユーザー名に入力できる文字数を超えています" . "<br>";
         }
     }
 
-    // 商品説明
-    if (empty($itemarray['item_desc'])) {
-        $errormessage .= "商品説明が未入力です" . "<br>";
+    // メールアドレス
+    if (empty($upduser['email'])) {
+        $errormessage .= "メールアドレスが未入力です" . "<br>";
     } else {
-        if (mb_strlen($itemarray['item_desc']) > 100) {
-            $errormessage .= "商品説明に入力できる文字数を超えています" . "<br>";
+        if (mb_strlen($upduser['email']) > 255) {
+            $errormessage .= "メールアドレスに入力できる文字数を超えています" . "<br>";
         }
     }
 
-    // 仕入先
-    if (empty($itemarray['item_comp'])) {
-        $errormessage .= "仕入先が未入力です" . "<br>";
+    // パスワード
+    if (empty($upduser['password'])) {
+        $errormessage .= "パスワードが未入力です" . "<br>";
     } else {
-        if (mb_strlen($itemarray['item_comp']) > 20) {
-            $errormessage .= "仕入先に入力できる文字数を超えています" . "<br>";
+        if (mb_strlen($upduser['password']) < 6 || mb_strlen($upduser['password']) >= 16) {
+            $errormessage .= "パスワードが6文字以上16文字未満にしてください" . "<br>";
+        }
+        if ($upduser['password'] !== $upduser['protpassword']) {
+            $errormessage .= "パスワードが一致しません<br>";
         }
     }
 
-    // 生産国
-    if (empty($itemarray['country'])) {
-        $errormessage .= "生産国が未入力です<br>";
-    } else {
-        if (mb_strlen($itemarray['country']) > 10) {
-            $errormessage .= "生産国に入力できる文字数を超えています" . "<br>";
+    // 権限 //
+    if (!empty($upduser['auth'])) {
+        //選択権限の存在チェック
+        $spval = $db->prepare('SELECT auth FROM m_auth WHERE auth=?');
+        $spval->execute(array($upduser['auth']));
+        $authid = $spval->fetch();
+
+        if ($authcontrol === "X") { //自分を選択（対象＝管理者）
+            if ($authid['auth'] !== $user['auth']) {
+                $errorauth = "あなたは有効な権限では、ありません<br>";
+            }
+        } else { //自分以外を選択
+            $authcnt = $db->prepare('SELECT COUNT(auth) AS cut FROM m_auth WHERE auth=?');
+            $authcnt->execute(array($upduser['auth']));
+            $acnt = $authcnt->fetch();
+
+            //権限選択が正しければ「真」
+            if ($acnt['cut'] !== "1") {
+                $errorauth = "権限が設定外です<br>";
+            }
         }
+    } else {
+        $errormessage .= "権限が未設定です<br>";
     }
 
-    // 価格
-    if (preg_match("/^[0-9]+$/", $itemarray['price'])) {
-        if (strlen($itemarray['price']) > 6) {
-            $errormessage .= "価格に設定できる金額を超えています" . "<br>";
-        }
-    } elseif (empty($itemarray['price'])) {
-        $errormessage .= "価格が未入力です" . "<br>";
-    } else {
-        $errormessage .= "価格は半角数字で入力してください" . "<br>";
-    }
-
-    // 仕入れ価格
-    if (preg_match("/^[0-9]+$/", $itemarray['w_price'])) {
-        if (strlen($itemarray['w_price']) > 6) {
-            $errormessage .= "仕入れ価格に設定できる金額を超えています" . "<br>";
-        }
-    } elseif (empty($itemarray['w_price'])) {
-        $errormessage .= '仕入れ価格が未入力です<br>';
-    } else {
-        $errormessage .= "仕入れ価格は半角数字で入力してください" . "<br>";
-    }
-
-    // 在庫数
-    if (preg_match("/^[0-9]+$/", $itemarray['stock'])) {
-        if (strlen($itemarray['stock']) > 3) {
-            $errormessage .= "在庫数に設定できる数量を超えています" . "<br>";
-        }
-    } elseif (empty($itemarray['stock'])) {
-        $errormessage .= "在庫数が未入力です" . "<br>";
-    } else {
-        $errormessage .= "在庫数は半角数字で入力してください" . "<br>";
-    }
-
-
-    // 入荷日
-    if (empty($itemarray['day'])) {
-        $errormessage .= "入荷日が未入力です" . "<br>";
-    } else {
-        // 文字列を分割
-        list($Y, $m, $d) = explode("-", $itemarray['day']);
-        if (!checkdate($m, $d, $Y)) {
-            $errormessage .= "入荷日に不正な日付が指定されています" . "<br>";
-        }
-    }
-
-    // 追加(登録)処理
-    if (empty($errormessage)) {
-        $statement = $db->prepare('INSERT INTO t_inventories SET item_name=?, item_desc=?, item_comp=?, country=?, price=?, w_price=?, stock=?, day=?, del_flg=0, ins_date=?, ins_user_id=?, upd_date=?, upd_user_id=?');
+    // ユーザー編集処理
+    if (empty($errormessage) && empty($errorauth)) {
+        //パスワードの暗号化
+        $hash_pass = password_hash($upduser['password'], PASSWORD_DEFAULT);
 
         //日時設定
         $today = new DateTime();
-        $instime = $today->format('Y-m-d H:i:s');
+        $updtime = $today->format('Y-m-d H:i:s');
 
-        $statement->execute(array($itemarray['item_name'], $itemarray['item_desc'], $itemarray['item_comp'],  $itemarray['country'],  $itemarray['price'], $itemarray['w_price'], $itemarray['stock'], $itemarray['day'], $instime,  $_SESSION['login']['user_id'], $instime,  $_SESSION['login']['user_id']));
+        $statement = $db->prepare('UPDATE m_users SET user_name=?, email=?, password=?, auth=?, upd_date=?, upd_user_id=? WHERE user_id=?');
 
-        $_SESSION['result'] = 1; //登録完了フラグ
-        header('Location: stock.php');
+        $statement->execute(array($upduser['user_name'], $upduser['email'], $hash_pass, $upduser['auth'],  $updtime,  $_SESSION['login']['user_id'], $upduser['userid']));
+
+        $_SESSION['result'] = 1; //更新完了フラグ
+        header('Location: users.php');
         exit();
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -114,81 +125,92 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width,">
-    <title>商品管理システム【商品登録画面】</title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <style>
-        html,
-        body {
-            height: 100;
-            text-align: center;
-            margin-top: 20px;
-        }
-    </style>
+    <title>商品管理システム【ユーザー更新画面】</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
-    <div>
-
-        <h1>商品登録</h1>
-        <hr>
+    <div style="font-size: 24px">
+        <h1>ユーザー編集</h1>
         <!-- エラーメッセージ -->
-        <?php if (!empty($errormessage)) : ?>
-            <p style="color: red; font-size: 20px;"><?php echo $errormessage; ?></p>
+        <?php if (!empty($errormessage) || !empty($errorauth)) : ?>
+            <p><?php echo $errormessage; ?></p>
+            <p><?php echo $errorauth; ?></p>
         <?php endif; ?>
+        <table>
+            <br>
+            <tr>
+                <td>
+                    <form method="post" action="">
+                        <input type="hidden" name="userid" value="<?php if (empty($_POST['userid'])) {
+                                                                        print(htmlspecialchars($user['user_id'], ENT_QUOTES));
+                                                                    } else {
+                                                                        print(htmlspecialchars($_POST['userid'], ENT_QUOTES));
+                                                                    }
+                                                                    ?>">
+                        <div style="font-size: 24px">
+                            <!-- ユーザー名 -->
+                            <strong>ユーザー名　　　　　　</strong>
+                            <input type="text" placeholder="ユーザー名を入力してください" name="user_name" maxlength="255" style="font-size: 18px; width: 500px;" value="<?php if (empty($_POST['user_name'])) {
+                                                                                                                                                                print(htmlspecialchars($user['user_name'], ENT_QUOTES));
+                                                                                                                                                            } else {
+                                                                                                                                                                print(htmlspecialchars($_POST['user_name'], ENT_QUOTES));
+                                                                                                                                                            }
+                                                                                                                                                            ?>">
 
+                            <br><br>
+                            <!-- メールアドレス -->
+                            <strong>メールアドレス　　　　</strong>
+                            <input type="text" placeholder="メールアドレスを入力してください" name="email" maxlength="255" style="font-size: 18px; width: 500px;" value="<?php if (empty($_POST['email'])) {
+                                                                                                                                                                print(htmlspecialchars($user['email'], ENT_QUOTES));
+                                                                                                                                                            } else {
+                                                                                                                                                                print(htmlspecialchars($_POST['email'], ENT_QUOTES));
+                                                                                                                                                            }
+                                                                                                                                                            ?>">
 
-        <form action="" method="post">
-            <div style="font-size: 24px;">
-                <!-- 商品名 -->
-                <strong style="width: 200px;">商品名　　　　　</strong>
-                <input type="text" placeholder="商品名を入力して下さい" name="item_name" maxlength="255" value="<?php print(htmlspecialchars($_POST['item_name'], ENT_QUOTES)); ?>" style="width: 500px">
+                            <br><br>
+                            <!-- パスワード -->
+                            <strong>パスワード　　　　　　</strong>
+                            <input type="text" placeholder="パスワードを設定してください" name="password" maxlength="255" style="font-size: 18px; width: 500px;">
 
-                <br><br>
-                <!-- 商品説明 -->
-                <strong style="width: 200px;">商品説明　　　　</strong>
-                <input type="text" placeholder="商品説明を入力して下さい" name="item_desc" maxlength="255" value="<?php print(htmlspecialchars($_POST['item_desc'], ENT_QUOTES)); ?>" style="width: 500px">
+                            <br><br>
+                            <!-- パスワード（確認） -->
+                            <strong>パスワード（確認）　　</strong>
+                            <input type="text" placeholder="確認のため設定したパスワードを入力してください" name="protpassword" maxlength="255" style="font-size: 18px; width: 500px;">
 
-                <br><br>
-                <!-- 仕入先 -->
-                <strong style="width: 200px;">仕入先　　　　　</strong>
-                <input type="text" placeholder="仕入先を入力して下さい" name="item_comp" maxlength="255" value="<?php print(htmlspecialchars($_POST['item_comp'], ENT_QUOTES)); ?>" style="width: 500px">
-                <br><br>
-                <!-- 生産国 -->
-                <strong style="width: 200px;">生産国　　　　　</strong>
-                <input type="text" placeholder="生産国を入力して下さい" name="country" maxlength="255" value="<?php print(htmlspecialchars($_POST['country'], ENT_QUOTES)); ?>" style="width: 500px">
+                            <br><br>
+                            <?php if ($authcontrol !== "X") : ?>
+                                <!-- 権限 -->
+                                <strong>権限　　　　　　　　　</strong>
+                                <select name="auth" style="font-size: 18px; width: 500px;">
+                                    <option value="0">選択</option>
 
-                <br><br>
-                <!-- 価格 -->
-                <strong style="width: 200px;">価格　　　　　　</strong>
-                <input type="text" placeholder="価格を入力して下さい" name="price" maxlength="255" value="<?php print(htmlspecialchars($_POST['price'], ENT_QUOTES)); ?>" style="width: 500px">
+                                    <!-- 権限マスタ内の権限をすべて表示 -->
+                                    <?php while ($selauth = $auths->fetch()) : ?>
 
-                <br><br>
-                <!-- 仕入れ価格 -->
-                <strong style="width: 200px;">仕入れ価格　　　</strong>
-                <input type="text" placeholder="仕入れ価格を入力して下さい" name="w_price" maxlength="255" value="<?php print(htmlspecialchars($_POST['w_price'], ENT_QUOTES)); ?>" style="width: 500px">
+                                        <option value="<?php print(htmlspecialchars($selauth['auth'], ENT_QUOTES)); ?>"><?php print(htmlspecialchars($selauth['auth_name'], ENT_QUOTES)); ?></option>
 
-                <br><br>
-                <!-- 在庫数 -->
-                <strong style="width: 200px;">在庫数　　　　　</strong>
-                <input type="text" placeholder="在庫数を入力して下さい" name="stock" maxlength="255" value="<?php print(htmlspecialchars($_POST['stock'], ENT_QUOTES)); ?>" style="width: 500px">
+                                    <?php endwhile; ?>
+                                </select>
+                            <?php else : ?>
+                                <input type="hidden" name="auth" value=<?php print(htmlspecialchars($user['auth'], ENT_QUOTES)); ?>>
+                            <?php endif; ?>
+                        </div>
 
-                <br><br>
-                <!-- 入荷日 -->
-                <strong style="width: 200px;">入荷日　　　　　</strong>
-                <input type="text" placeholder="入荷日を入力して下さい(年-月-日：yyyy-mm-dd)" name="day" maxlength="255" value="<?php print(htmlspecialchars($_POST['day'], ENT_QUOTES)); ?>" style="width: 500px">
+                        <br><br>
 
-            </div>
-
-            <br><br>
-
-            <div>
-                <!-- 登録ボタン -->
-                <input type="submit" value="登録" style="font-size: 30px; width: 150px; height: 50px;">
-            </div>
-        </form>
+                        <div>
+                            <!-- 登録ボタン -->
+                            <input type="submit" value="更新" style="font-size: 30px; width: 150px; height: 50px;">
+                        </div>
+                    </form>
+                </td>
+            </tr>
+        </table>
     </div>
-    <a href="stock.php">
-        <p style="margin-left: 20%; text-align: left;">≪ 戻る</p>
+    <a href="users.php">
+        <p class="backtab">≪ 戻る</p>
     </a>
 </body>
 
